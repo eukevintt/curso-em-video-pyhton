5e30947821572d5572a171c45fbd8ad37c2a7759
Kevin Araujo <kevinaraujoo677@gmail.com>
2021-07-14 15:45:02 -0300

    Aula 06 concluida

---
+++ Mundo 1/Aula6/aula06a.py
@@ -0,0 +1,4 @@
+n1 = input('Digite um valor: ')
+n2 = input('Digite outro: ')
+soma = int(n1) + int(n2)
+print('A soma vale entre {} e {} vale {}'.format(n1, n2, soma))
---
+++ Mundo 1/Aula6/desafio004.py
@@ -0,0 +1,2 @@
+algo = input('Digite algo: ')
+print('O tipo dele é {}. Ele é Alfa Númerico? {}. Ele é Númerico? {}. Ele é alfabetico? {}.'.format(type(algo), algo.isalnum(), algo.isnumeric(), algo.isalpha()))
