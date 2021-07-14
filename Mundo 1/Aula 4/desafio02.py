6599ae7e977bd54a25e386db3a7d4718438cb0c2
Kevin Araujo <kevinaraujoo677@gmail.com>
2021-07-14 11:26:01 -0300

    Aula 04 concluida

---
+++ Mundo 1/Aula 4/desafio02.py
@@ -0,0 +1,5 @@
+print('===== DESAFIO 02 =====')
+dia = input('Qual o dia do seu aniversário?')
+mes = input('Qual o mês do seu aniversário?')
+anonasc = input('Qual seu ano de nascimento?')
+print('Você nasceu no dia', dia, 'de', mes, 'de', anonasc, '. Correto?')
---
+++ Mundo 1/Aula 4/teste01.py
@@ -0,0 +1,4 @@
+nome = input('Qual o seu nome?')
+idade = input('Qual é a sua idade?')
+peso = input('Qual é o seu peso?')
+print('O seu nome é', nome, 'com', idade, 'anos e pesa', peso,'kg')
