466d2b8878d9057094d5e224ff1f0d78b2dabffc
Kevin Araujo <kevinaraujoo677@gmail.com>
2021-07-14 15:13:36 -0300

    Aula Exercicio 02 completa

---
+++ Mundo 1/PythonExercicios/ex002.py
@@ -0,0 +1,6 @@
+print('===== EXERCICIO 02 =====')
+print('- Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas -')
+
+nome = input('Qual o seu nome?')
+
+print('É um prazer te conhecer, {}!'.format(nome))
